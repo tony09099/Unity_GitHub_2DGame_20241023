32288cebc405cc59c3d3a610869afebe1daff982
Fidowen <87049858+tony09099@users.noreply.github.com>
2024-10-23 16:31:35 +0800

    Update 新增場景控制器

---
+++ Assets/SceneControl.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneControl : MonoBehaviour
+{
+   public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
